005830a4c93e496978622fc397f24dbdd2b83df1
da <da@localhost.localdomain>
2019-04-15 16:46:32 +0300

    Minor changes

--- db/migrate/20110102015152_create_partnerships.rb
+++ db/migrate/20110102015152_create_partnerships.rb
@@ -1,7 +1,7 @@
 class CreatePartnerships < ActiveRecord::Migration[5.2]
   def change
     create_table :partnerships do |t|
-      t.references :person, foreign_key: true
+      t.references :person,  foreign_key: true
       t.references :partner, foreign_key: false
       t.date :date_started
       t.date :date_ended
