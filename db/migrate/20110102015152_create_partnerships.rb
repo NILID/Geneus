d3dfd28599e9399d1c4532f6f18739f5eaa5693f
da <da@localhost.localdomain>
2019-04-15 16:35:46 +0300

    Foreign_key to false

--- db/migrate/20110102015152_create_partnerships.rb
+++ db/migrate/20110102015152_create_partnerships.rb
@@ -2,7 +2,7 @@ class CreatePartnerships < ActiveRecord::Migration[5.2]
   def change
     create_table :partnerships do |t|
       t.references :person, foreign_key: true
-      t.references :partner, foreign_key: true
+      t.references :partner, foreign_key: false
       t.date :date_started
       t.date :date_ended
       t.string :nature
