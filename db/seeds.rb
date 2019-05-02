6a0d7a1d758365f6f40ffc97cc91baff581ef7c9
da <dm.ilin.aleksandrovich@gmail.com>
2019-05-03 00:03:16 +0300

    Add anton capolla

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,7 @@
 # creating persons
 italia  = Person.create(name: 'Italia Pennino',       gender: 'female', date_of_birth: '1912-12-12', date_of_death: '2004-01-21')
 carmine = Person.create(name: 'Carmine Coppola',      gender: 'male'  , date_of_birth: '1910-11-06', date_of_death: '1991-04-26', bio: 'American composer, flautist, editor, musical director, and songwriter.')
+# anton   = Person.create(name: 'Anton Coppola',        gender: 'male'  , date_of_birth: '1917', bio: 'American opera conductor and composer')
 joy     = Person.create(name: 'Joy Vogelsang',        gender: 'female')
 august  = Person.create(name: 'August Floyd Coppola', gender: 'male'  , date_of_birth: '1934-02-16', date_of_death: '2009-10-27', bio: 'American academic, author, film executive and advocate for the arts.')
 nicolas = Person.create(name: 'Nicolas Kim Coppola',  gender: 'male'  , date_of_birth: '1964-01-07', bio: 'Known professionally as Nicolas Cage, is an American actor, director and producer.')
